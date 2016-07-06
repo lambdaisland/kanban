921130af18aa2cac0e7e70eef11de9879e800f13
Arne Brasseur <arne@arnebrasseur.net>
2016-07-06 18:44:17 +0200

    30

--- src/cljs/kanban/scratch.cljs
+++ src/cljs/kanban/scratch.cljs
@@ -1,16 +1,2 @@
 (ns kanban.scratch
   (:require [reagent.core :as r]))
-
-(def board (r/atom {:columns [{:cards []}
-                              {:cards [{:title "Hello Island!"}]}]}))
-
-
-(def cards-cursor
-  (r/cursor board [:columns 0 :cards]))
-
-@cards-cursor
-
-(swap! cards-cursor conj {:title "New card in column 0"})
-
-@cards-cursor ;;=> [{:title "New card in column 0"}]
-@board ;;=> {:columns [{:cards [{:title "New card in column 0"}]} {:cards [{:title "Hello Island!"}]}]}
