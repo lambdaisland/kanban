6ebd3c245a8d986c5e0f7c759bc921b25cb56a23
Arne Brasseur <arne@arnebrasseur.net>
2016-07-06 16:34:39 +0200

    5

--- src/cljs/kanban/scratch.cljs
+++ src/cljs/kanban/scratch.cljs
@@ -1,18 +1,6 @@
 (ns kanban.scratch
   (:require [reagent.core :as r]))
 
-(def card-atom (r/atom {:title "Hello Island!"}))
-
-(defn set-title! [card title]
-  (swap! card (fn [c] (assoc c :title title))))
-
-card-atom ;;=> #<Atom: {:title "Hello Island!"}>
-
-(set-title! card-atom "Hello Turtles!")
-
-card-atom ;;=> #<Atom: {:title "Hello Turtles!"}>
-
-
 (def board (r/atom {:columns [{:cards []}
                               {:cards [{:title "Hello Island!"}]}]}))
 
@@ -21,3 +9,10 @@ card-atom ;;=> #<Atom: {:title "Hello Turtles!"}>
 (get-in @board [:columns 1])        ;;=> {:cards [{:title "Hello Island!"}]}
 (get-in @board [:columns 1 :cards]) ;;=> [{:title "Hello Island!"}]
 (get-in @board [:columns 1 :cards 0 :title]) ;;=> "Hello Island!"
+
+
+board ;;=> #<Atom: {:columns [{:cards []} {:cards [{:title "Hello Island!"}]}]}>
+
+(swap! board assoc-in [:columns 1 :cards 0 :title] "Hello Turtles")
+
+board ;;=> #<Atom: {:columns [{:cards []} {:cards [{:title "Hello Turtles"}]}]}>
