d8fbd294a64fa72b5c2dea5f1fdbd16ca891dbd5
Arne Brasseur <arne@arnebrasseur.net>
2016-07-06 18:45:27 +0200

    31

--- src/cljs/kanban/scratch.cljs
+++ src/cljs/kanban/scratch.cljs
@@ -1,2 +1,12 @@
 (ns kanban.scratch
   (:require [reagent.core :as r]))
+
+(defn Form1Component [x y z]
+  [:div.f1 x y z])
+
+(defn Form2Component [x y z]
+  (let [component-state (r/atom {})]
+    (request-data-from-api :handler (fn [response] (reset! component-state response)))
+
+    (fn [x y z]
+      [:div.f2 x y z])))
