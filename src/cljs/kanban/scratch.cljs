422944cd4d972efea3ffc85cba988c5fadb63bf4
Arne Brasseur <arne@arnebrasseur.net>
2016-07-06 18:46:26 +0200

    32

--- src/cljs/kanban/scratch.cljs
+++ src/cljs/kanban/scratch.cljs
@@ -5,8 +5,10 @@
   [:div.f1 x y z])
 
 (defn Form2Component [x y z]
-  (let [component-state (r/atom {})]
-    (request-data-from-api :handler (fn [response] (reset! component-state response)))
+  (fn [x y z]
+    [:div.f2 x y z]))
 
-    (fn [x y z]
-      [:div.f2 x y z])))
+(defn Form3Component [x y z]
+  (r/create-class {:display-name "Form3Component"
+                   :reagent-render (fn [x y z]
+                                     [:div.f3 x y z])}))
