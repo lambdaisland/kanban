25be063e6fea7b70daa296b254bc01b638e43b83
Arne Brasseur <arne@arnebrasseur.net>
2016-07-06 16:30:37 +0200

    2

--- src/cljs/kanban/scratch.cljs
+++ src/cljs/kanban/scratch.cljs
@@ -2,3 +2,12 @@
   (:require [reagent.core :as r]))
 
 (def card-atom (r/atom {:title "Hello Island!"}))
+
+(defn set-title! [card title]
+  (swap! card (fn [c] (assoc c :title title))))
+
+card-atom ;;=> #<Atom: {:title "Hello Island!"}>
+
+(set-title! card-atom "Hello Turtles!")
+
+card-atom ;;=> #<Atom: {:title "Hello Turtles!"}>
